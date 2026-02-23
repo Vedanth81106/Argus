c1d32ec9f978247a6ddb3a653458d25788457f7b
Vedanth <vedanth81106@gmail.com>
2026-02-23 23:46:23 +0530

    Added messenger.py and refactored files

---
+++ apps/worker/messenger.py
@@ -0,0 +1,23 @@
+import pika
+import json
+import os
+
+def send_ai_feedback(channel, repo_id, commit_sha, ai_feedback):
+
+    result_payload = {
+
+        "repoId": repo_id,
+        "commitSha": commit_sha,
+        "aiFeedback": ai_feedback
+    }
+
+    channel.basic_publish(
+        exchange='',
+        routing_key='results-queue',
+        body=json.dumps(result_payload),
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        )
+    )
+
+    print(f"Sent AI feedback for repo {repo_id}, commit {commit_sha} to results-queue.")
